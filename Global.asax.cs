3dc5b2bf3e10ec782a6ef703e7b07f9e5a213a3b
huydt,bdh <huydt.bdh@vnpt.vn>
2023-09-19 00:27:23 +0700

    update bo hien thi trang error

--- Global.asax.cs
+++ Global.asax.cs
@@ -59,30 +59,30 @@ namespace TLBD
             //HttpRuntime.Cache["luottruycap_homnay"] = luottruycap_homnay;
         }
 
-        public void Application_Error(Object sender, EventArgs e)
-        {
-            Exception exception = Server.GetLastError();
-            Server.ClearError();
+        //public void Application_Error(Object sender, EventArgs e)
+        //{
+        //    Exception exception = Server.GetLastError();
+        //    Server.ClearError();
 
-            var routeData = new RouteData();
-            routeData.Values.Add("controller", "ErrorPage");
-            routeData.Values.Add("action", "Error");
-            routeData.Values.Add("exception", exception);
+        //    var routeData = new RouteData();
+        //    routeData.Values.Add("controller", "ErrorPage");
+        //    routeData.Values.Add("action", "Error");
+        //    routeData.Values.Add("exception", exception);
 
-            if (exception.GetType() == typeof(HttpException))
-            {
-                routeData.Values.Add("statusCode", ((HttpException)exception).GetHttpCode());
-            }
-            else
-            {
-                routeData.Values.Add("statusCode", 500);
-            }
+        //    if (exception.GetType() == typeof(HttpException))
+        //    {
+        //        routeData.Values.Add("statusCode", ((HttpException)exception).GetHttpCode());
+        //    }
+        //    else
+        //    {
+        //        routeData.Values.Add("statusCode", 500);
+        //    }
 
-            Response.TrySkipIisCustomErrors = true;
-            IController controller = new ErrorPageController();
-            controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
-            Response.End();
-        }
+        //    Response.TrySkipIisCustomErrors = true;
+        //    IController controller = new ErrorPageController();
+        //    controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
+        //    Response.End();
+        //}
 
 
         protected void Application_BeginRequest()
